c8b40be8cc7a4e270d7b5fd41db5d5e2f827ebeb
Rizal <mrizalpahlevi372@gmail.com>
2022-01-06 11:27:41 +0700

    p13

---
+++ p13/menusamping.php
@@ -0,0 +1,37 @@
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu">                
+                  <li class="active">
+                      <a class="" href="index.php?page=dashboard">
+                          <i class="icon_house_alt"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+				  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Masters</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="index.php?page=bukutamu">Buku Tamu</a></li>                          
+                          <li><a class="" href="index.php?page=berita">Berita</a></li>
+                      </ul>
+                  </li> 
+					<li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Transaction</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="index.php?page=order">Order</a></li>                          
+                          <li><a class="" href="index.php?page=payment">Payment</a></li>
+                      </ul>
+                  </li> 				  
+                  
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
